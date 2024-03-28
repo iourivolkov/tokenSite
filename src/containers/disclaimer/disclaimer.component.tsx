493944b3909634b63cddda2184475a747c540991
Iouri Volkov <volk.iouri@gmail.com>
2024-03-28 00:48:41 -0400

    Add consistent padding to hero and tokenomics sections, adjust copy in hero section, add quack sound on button click to footer component

--- src/containers/disclaimer/disclaimer.component.tsx
+++ src/containers/disclaimer/disclaimer.component.tsx
@@ -7,7 +7,7 @@ import {
 
 const disclaimerText = {
   heading: "Disclaimer",
-  subheading: "Lurry didn't read, but you should.",
+  subheading: "Lurry didn't read this, but you should",
   bodyText: `$Lurry was created by lab-engineered rubber ducks who have no knowledge of economics or monetary systems. As such, $Lurry has no value and is a meme coin meant for entertainment purposes only. Do not risk money you are afraid of losing. Just like a duck bobbing in the water, the price may go up or it may go down. We take no responsibility for the price action. `,
 };
 
