08048517aad023805b299f212717b3d82e95eb03
Iouri Volkov <volk.iouri@gmail.com>
2024-03-27 23:27:03 -0400

    Clean up disclaimer copy, put copy into its own object

--- src/containers/disclaimer/disclaimer.component.tsx
+++ src/containers/disclaimer/disclaimer.component.tsx
@@ -5,19 +5,18 @@ import {
   Text,
 } from "./disclaimer.styled.ts";
 
+const disclaimerText = {
+  heading: "Disclaimer",
+  subheading: "Lurry didn't read, but you should.",
+  bodyText: `$Lurry was created by lab-engineered rubber ducks who have no knowledge of economics or monetary systems. As such, $Lurry has no value and is a meme coin meant for entertainment purposes only. Do not risk money you are afraid of losing. Just like a duck bobbing in the water, the price may go up or it may go down. We take no responsibility for the price action. `,
+};
+
 export const DisclaimerContainer = () => {
   return (
     <LorePageContainer>
-      <Heading>Disclaimer</Heading>
-      <Subheading>Lurry can't read, but you should.</Subheading>
-      <Text>
-        $Lurry was created by lab-engineered rubber ducks who have no knowledge
-        of economics or monetary systems. As such, $lurry has no value and is a
-        meme coin meant for entertainment purposes only. Do not risk money you
-        are afraid of losing. Just like a Lurry bobbing in the pond, the price
-        may go up or it may go down. We take no responsibility for the price
-        action.
-      </Text>
+      <Heading>{disclaimerText.heading}</Heading>
+      <Subheading>{disclaimerText.subheading}</Subheading>
+      <Text>{disclaimerText.bodyText}</Text>
     </LorePageContainer>
   );
 };
