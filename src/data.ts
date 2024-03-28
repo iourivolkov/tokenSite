fe7be0e2c2fac811439348d8faf4e4d793411d04
Viktor Avelino <viktor.avelino@gmail.com>
2024-03-28 16:58:55 -0400

    add data file

---
+++ src/data.ts
@@ -0,0 +1,5 @@
+export const data = {
+  tokenContract: "432xjvh7zqh85f669hr7ofegkd5cbzd4m3jn41uztc2d",
+  buyLink:
+    "https://dexscreener.com/solana/432xjvh7zqh85f669hr7ofegkd5cbzd4m3jn41uztc2d",
+};
