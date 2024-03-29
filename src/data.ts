f66486cb7edf6d1cbf7d1075f02c248c987db77b
Viktor Avelino <viktor.avelino@gmail.com>
2024-03-28 20:03:35 -0400

    contract

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export const data = {
   tokenContract: "6DEbzgDNQYkgk7sFZ1cTBjeUK9gpsLjgURY9EtHkpyCJ",
   buyLink:
-    "https://dexscreener.com/solana/432xjvh7zqh85f669hr7ofegkd5cbzd4m3jn41uztc2d",
+    "https://dexscreener.com/solana/6DEbzgDNQYkgk7sFZ1cTBjeUK9gpsLjgURY9EtHkpyCJ",
 };
